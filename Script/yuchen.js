d310056c5a3e0b63bbcfb340f221905c02fac0bf
owl <42443925+owlm00n@users.noreply.github.com>
2024-05-28 20:29:49 +0800

    Update yuchen.js

--- Script/yuchen.js
+++ Script/yuchen.js
@@ -82,7 +82,7 @@ else {
 function GetCookie() {
   const TM = $.read("TIME");
   const CK = $request.headers['Cookie'] || $request.headers['cookie'];
-  if (CK) {
+  if (CK && (CK.includes('Hm_lpvt') || CK.includes('Hm_lvt'))) {
     $.write(CK, "COOKIE");
     if (!TM || TM && (Date.now() - TM) / 1000 >= 21600) {
       $.notify("ios yuchen", "", `写入Cookie成功 🎉`);
